6c3e6258776159d491add5bcb93c66f91be0b64b
[vishwaja] <[vishwajameddela18@msitprogram.net]>
2021-07-20 21:27:43 +0530

    Done

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -8,6 +8,10 @@
 
 import math
 def fun_find_int_roots(a, b, c):
-	return 0, 0
+	root1 = int((-b + math.sqrt(b**2 - 4*a*c))//2*a)
+	root2 = int((-b - math.sqrt(b**2 - 4*a*c))//2*a)
+	if root1 < root2:
+		return root1, root2
+	return root2, root1
 
 
