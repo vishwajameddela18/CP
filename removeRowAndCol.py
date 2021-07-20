d5f23dd6828cf81bfe66c430717b281416d220ab
[vishwaja] <[vishwajameddela18@msitprogram.net]>
2021-07-20 16:03:58 +0530

    Done

--- removeRowAndCol.py
+++ removeRowAndCol.py
@@ -23,6 +23,16 @@
 
 def removeRowAndCol(L, row, col):
     # Your code goes here...
-    pass
+    if len(L) > 1 or row > 0 or col > 0:
+        for i in range(len(L)):
+            del L[i][col]
+        L.pop(row)
+        return L
+    # pass
 
 # Write your own test cases.
+
+L = [ [ 2, 3, 4, 5], [ 8, 7, 6, 5], [ 0, 1, 2, 3] ]
+row = 1
+col = 2
+print(removeRowAndCol(L, row, col))
