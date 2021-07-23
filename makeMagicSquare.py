31f4688c17e1458339125077bdc874eb483f37eb
[vishwaja] <[vishwajameddela18@msitprogram.net]>
2021-07-23 09:32:34 +0530

    Done

--- makeMagicSquare.py
+++ makeMagicSquare.py
@@ -35,4 +35,41 @@
 
 def makeMagicSquare(n):
     # Your code goes here...
-    pass
+
+    if n< 1 or n%2 == 0:
+        return None
+    else:
+        magicSquare = [[0 for x in range(n)] for y in range(n)]
+
+        i = n / 2
+        j = n - 1
+
+        num = 1
+        while num <= (n * n):
+            if i == -1 and j == n: 
+                j = n - 2
+                i = 0
+            else:
+    
+                if j == n:
+                    j = 0
+
+                if i < 0:
+                    i = n - 1
+    
+            if magicSquare[int(i)][int(j)]:  
+                j = j - 2
+                i = i + 1
+                continue
+            else:
+                magicSquare[int(i)][int(j)] = num
+                num = num + 1
+    
+            j = j + 1
+            i = i - 1  
+    
+        return magicSquare
+
+ 
+    # pass
+print(makeMagicSquare(7))
