9d4301d7ae2549e9ab3364a42d3b6bf85e70fb7b
[vishwaja] <[vishwajameddela18@msitprogram.net]>
2021-07-26 08:34:58 +0530

    Done_1

---
+++ is_add_prime_1.py
@@ -0,0 +1,17 @@
+# sAdditivePrime: Additive primes can be defined as prime numbers where the sum of its digits is a prime number. Write a function isAdditivePrime that takes n as an integer and returns True if n is an Additive Prime and False otherwise.
+# Some of the Additive Primes are 2, 3, 5, 7, 11, 23, 29, and etc.
+# 29 = 2 + 9 = 11 = 1 + 1 = 2 and 2 is a prime number.
+# Here are sample test cases:
+# assert (isAdditivePrime(2) == True)
+# assert (isAdditivePrime(3) == True)
+# assert (isAdditivePrime(5) == True)
+# assert (isAdditivePrime(13) == False)
+# assert (isAdditivePrime(23) == True)
+# assert (isAdditivePrime(29) == True)
+# assert (isAdditivePrime(41) == True)
+# assert (isAdditivePrime(98) == False)
+# assert (isAdditivePrime(198) == False)
+# assert (isAdditivePrime(290) == False)
+# assert (isAdditivePrime(67) == True)
+# assert (isAdditivePrime(97) == False)
+# print("All test cases passed... :-)")
