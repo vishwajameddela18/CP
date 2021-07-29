dc7b8fe280a260a85cc007cfc04671c1453666e4
[vishwaja] <[vishwajameddela18@msitprogram.net]>
2021-07-29 08:13:09 +0530

    Done -1

---
+++ 02-binarysearch-Python/isPalindrome.py
@@ -0,0 +1,12 @@
+# isPalindromicPrime() Write a function isPalindromicPrime that takes a value n as a parameter and returns True if the given n is a palindrome and prime and False otherwise.
+# assert (isPalindromicPrime(2) == True)
+# assert (isPalindromicPrime(10) == False)
+# assert (isPalindromicPrime(104) == False)
+# assert (isPalindromicPrime(235) == False)
+# assert (isPalindromicPrime(131) == True)
+# assert (isPalindromicPrime(900) == False)
+# assert (isPalindromicPrime(101) == True)
+# assert (isPalindromicPrime(383) == True)
+# assert (isPalindromicPrime(373) == True)
+# assert (isPalindromicPrime(121) == False)
+# print("All test cases passed... :-)")
