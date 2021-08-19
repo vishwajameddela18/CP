43da26297b3d32fd1f6e335fc9d929e9a22fb51e
[vishwaja] <[vishwajameddela18@msitprogram.net]>
2021-08-19 18:25:53 +0530

    Done - p

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -11,4 +11,27 @@
 
 def shortenlongruns(L, k):
 	# Your code goes here
-	pass
+	i = 0
+	while(True):
+		
+		count = L.count(L[i])
+		if count >= k:
+			print(1)
+			for j in range(abs(count-k+1)):
+				flag = False
+				if L[i] != L[i+1]:
+					flag = True
+				
+				if flag:
+					break
+				L.remove(L[i])
+				
+		# print(len(L))
+		# print(1)
+		
+		i+= 1
+		if len(L)-1<= i:
+			break
+	return L
+	# pass
+# print(shortenlongruns([2, 3, 5, 5, 5,5,5,5, 3], 3))
